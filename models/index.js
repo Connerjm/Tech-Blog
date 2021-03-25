5e9515a03ded5bf30a4d664207de14b5604797ae
Conner Martin <connerjm2@gmail.com>
2021-03-24 20:41:25 -0700

    Added models files.

---
+++ models/index.js
@@ -0,0 +1,11 @@
+/* Imports */
+
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+/* Relationships */
+
+/* Exports */
+
+module.exports = { User, Post, Comment };
