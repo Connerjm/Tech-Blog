d05b172041ce4a8403b02dac49b67d01baf407b0
Conner Martin <connerjm2@gmail.com>
2021-03-27 16:05:05 -0700

    Changed up the sql file a bit.

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,8 @@ DROP DATABASE IF EXISTS tech_blog_db;
 -- Make the database. --
 CREATE DATABASE tech_blog_db;
 
+-- For testing. --
 USE tech_blog_db;
 
-SELECT * FROM user;
-
-SELECT * FROM post;
-
-SELECT * FROM comment;
-
+-- Basic select for users with posts. --
 SELECT username, title AS Title, content AS Body, post.createdAt AS Date FROM user JOIN post ON user.id = post.author_id;
