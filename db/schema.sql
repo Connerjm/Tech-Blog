216e1bb677525ff4eb383c5cb87487d19e1f9ad3
Conner Martin <connerjm2@gmail.com>
2021-03-26 21:29:03 -0700

    Updating db, still working on seed.

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,11 @@ DROP DATABASE IF EXISTS tech_blog_db;
 
 -- Make the database. --
 CREATE DATABASE tech_blog_db;
+
+USE tech_blog_db;
+
+SELECT * FROM user;
+
+SELECT * FROM post;
+
+SELECT * FROM comment;
