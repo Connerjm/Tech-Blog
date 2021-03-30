9ed531a445891d7cabd7ea0f71c07ac990a8e1fb
Conner Martin <connerjm2@gmail.com>
2021-03-29 20:31:00 -0700

    Can now serve posts on the homepage.

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,26 @@
 const router = require("express").Router();
+const { Post, User } = require("../models");
 
-router.get("/", (req, res) =>
+router.get("/", async (req, res) =>
 {
-    res.render("home");
+    try
+    {
+        const postData = await Post.findAll({
+            include:
+            [{
+                model: User,
+                attributes: ["username"]
+            }]
+        });
+
+        const posts = postData.map(post => post.get({ plain: true }));
+
+        res.render("home", { posts });
+    }
+    catch (err)
+    {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
