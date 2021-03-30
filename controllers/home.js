7425791f10564ce3d1683eadd6448f5953474aff
Conner Martin <connerjm2@gmail.com>
2021-03-29 19:41:49 -0700

    Finished the first pass at the view files. Can render the homepage successfully.

--- controllers/home.js
+++ controllers/home.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 router.get("/", (req, res) =>
 {
-    res.status(200).json({ message: "Hey look! It works!" });
+    res.render("home");
 });
 
 module.exports = router;
